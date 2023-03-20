2c39ddbe635a0f78ccaac19d3fd410b4090dd86c
Veronique Cadet <cadetvero@gmail.com>
2023-03-20 17:25:29 -0400

    messages

--- brtr/src/Components/Messages.jsx
+++ brtr/src/Components/Messages.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import NavBarTwo from "./NavBarTwo";
 import Footer from "./Footer";
 import { useLocation } from "react-router-dom";
+import dayjs from "dayjs";
 
 const ws = new WebSocket("ws://localhost:3000/cable");
 
@@ -85,53 +86,71 @@ function Messages({ setUser, user }) {
   };
 
   return (
-    <div className="h-screen">
+    <div className="h-screen ">
       <NavBarTwo setUser={setUser} />
-      <section className="pt-24 pb-36 bg-white  h-screen">
-        <div className="container px-4 mx-auto">
+      <section className="pt-3 pb-10 h-screen container p-5 mx-auto my-10 ">
+        {/* <div className="container px-4 mx-auto">
           <p className="mb-6 text-sm text-indigo-600 text-center font-bold uppercase tracking-px">
             {user?.first_name} {user?.last_name}
           </p>
-          {/* <h2 className="mb- text-6xl md:text-8xl xl:text-10xl text-center font-bold font-heading tracking-px-n leading-none">
-            Your Messages
-          </h2> */}
-        </div>
-      
-        <div className="flex justify-center mt-5 mb-24 ">
-          <div className="bg-slate-100 px-10 py-5 w-2/6   h-5/6 rounded-xl ">
-            <div className="max-h-96">
+        </div>  */}
+
+        <div className="flex mt-5  w-full h-full  border-2 border-slate-100 rounded-tl-3xl rounded-tr-3xl">
+          <div className="bg-white w-2/6  border-r-2 border-slate-100 rounded-tl-3xl">
+            <div className=" border-b-2  h-10 w-full border-slate-100  ">
+              <h1 className="  text-2xl text-center font-normal text-indigo-700 w-full ">
+                Messages
+              </h1>
+            </div>
+          </div>
+          <div className="bg-white  pb-5 w-full h-full  rounded-tr-3xl ">
+            <div className="border-b-2  h-10 w-full border-slate-100 rounded-tr-3xl ">
               <h1 className="text-center text-indigo-700 font-bold">
                 Messages
               </h1>
               <p className="text-center text-indigo-700">Guid: {guid}</p>
             </div>
-            <div className="bg-white py-2 px-2 mt-2 mb-5 rounded-xl h-96 overflow-scroll">
+            <div className=" py-10 px-64  h-5/6 overflow-scroll">
               {messages.map((m) => (
                 <div className="max-h-96 " key={m?.id}>
-                  <p className="text-indigo-700">{m?.messanger?.first_name}</p>
-                  <p className="ml-5">{m?.message}</p>
+                  <div className="flex mb-7">
+                    <img
+                      className="h-13 w-13 rounded-full"
+                      src={m?.messanger?.picture}
+                    />
+                    <div>
+                      <div className="flex items-center ">
+                        <p className="text-indigo-700 font-bold text-xl ml-5">
+                          {m?.messanger?.first_name}
+                        </p>
+                        <p className=" text-sm ml-2 text-slate-700">
+                          {dayjs(m?.created_at).format("MMM-DD h:mm A")}
+                        </p>
+                      </div>
+                      <p className="ml-5">{m?.message}</p>
+                    </div>
+                  </div>
                 </div>
               ))}
             </div>
-            <div className=" ">
+            <div className="">
               <form
-                className="flex"
+                className="flex justify-center "
                 onSubmit={(e) => {
                   e.preventDefault();
                   handleSubmit();
-                  setMessage("");
                   fetchMessages();
                 }}
               >
                 <input
                   value={message}
                   onChange={(e) => setMessage(e.target.value)}
-                  className="w-96 h-10 border-2 "
+                  className=" w-4/6 h-13 border-2  "
                   type="text"
                   name="message"
                 />
                 <button
-                  className="ml-3 inline-block px-6 py-3 leading-none text-white rounded shadow bg-amber-500 hover:bg-indigo-600 hover:-translate-y-1"
+                  className="ml-3 inline-block px-6  leading-none text-white rounded shadow bg-amber-500 hover:bg-indigo-600 hover:-translate-y-1"
                   type="submit"
                 >
                   Send
