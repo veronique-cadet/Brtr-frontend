5f73a62215c51f9287e3779fe43caf708af876e6
Veronique Cadet <cadetvero@gmail.com>
2023-05-25 17:45:52 -0400

    need to finish mobile

--- brtr/src/Components/HowItWorks.jsx
+++ brtr/src/Components/HowItWorks.jsx
@@ -20,7 +20,7 @@ function HowItWorks() {
             </div>
           </div>
           <div className="flex flex-wrap -m-3">
-            <div className="w-full md:w-1/3 p-3">
+            <div className="w-1/3 p-3">
               <a className="flex justify-center md:block h-full" href="">
                 <div className="relative h-full rounded-3xl overflow-hidden">
                   <img
@@ -28,15 +28,15 @@ function HowItWorks() {
                     src="https://images.unsplash.com/photo-1615339725569-80172615f345?crop=entropy&amp;cs=tinysrgb&amp;fm=jpg&amp;ixid=MnwzMzIzMzB8MHwxfHNlYXJjaHwxN3x8cGFpbnRpbmd8ZW58MHx8fHwxNjc3MTIwMDU3&amp;ixlib=rb-4.0.3&amp;q=80&amp;w=1920"
                     alt=""
                   />
-                  <div className="absolute bottom-0 left-0 w-full px-11 pb-10">
+                  <div className="absolute bottom-0 left-0 w-full px-2 py-6 lg:px-11 lg:py-10">
                     <div
-                      className="px-8 py-4 bg-white bg-opacity-10 rounded-xl shadow-5xl"
+                      className="px-2 py-2 lg:px-8 lg:py-4 bg-white bg-opacity-10 rounded-xl shadow-5xl"
                       style={{
                         webkitBackdropFilter: "blur(5px)",
                         backdropFilter: "blur(5px)",
                       }}
                     >
-                      <h3 className="text-lg text-white text-center font-semibold">
+                      <h3 className="text-sm lg:text-lg text-white text-center font-semibold">
                         Everyone has a skill! Share yours on Bartr!
                       </h3>
                     </div>
@@ -44,7 +44,7 @@ function HowItWorks() {
                 </div>
               </a>
             </div>
-            <div className="w-full md:w-1/3 p-3">
+            <div className="w-1/3 p-3">
               <a className="flex justify-center md:block h-full" href="#">
                 <div className="relative h-full rounded-3xl overflow-hidden">
                   <img
@@ -52,15 +52,15 @@ function HowItWorks() {
                     src="https://images.unsplash.com/photo-1607459726370-d1abfcda1370?crop=entropy&amp;cs=tinysrgb&amp;fm=jpg&amp;ixid=MnwzMzIzMzB8MHwxfHNlYXJjaHw3fHxvbiUyMGNvbXB1dGVyfGVufDB8fHx8MTY3NzE2NjE4NA&amp;ixlib=rb-4.0.3&amp;q=80&amp;w=1920"
                     alt=""
                   />
-                  <div className="absolute bottom-0 left-0 w-full px-11 pb-10">
+                  <div className="absolute bottom-0 left-0 w-full px-2 py-6 lg:px-11 lg:py-10">
                     <div
-                      className="px-8 py-4 bg-white bg-opacity-10 rounded-xl shadow-5xl"
+                      className="px-2 py-2 lg:px-8 lg:py-4 bg-white bg-opacity-10 rounded-xl shadow-5xl"
                       style={{
                         webkitBackdropFilter: "blur(5px)",
                         backdropFilter: "blur(5px)",
                       }}
                     >
-                      <h3 className="text-lg text-white text-center font-semibold">
+                      <h3 className="text-sm lg:text-lg text-white text-center font-semibold">
                         Browse skills you would like to learn from others!
                       </h3>
                     </div>
@@ -68,7 +68,7 @@ function HowItWorks() {
                 </div>
               </a>
             </div>
-            <div className="w-full md:w-1/3 p-3">
+            <div className=" w-1/3 p-3">
               <a className="flex justify-center md:block h-full" href="#">
                 <div className="relative h-full rounded-3xl overflow-hidden">
                   <img
@@ -76,15 +76,15 @@ function HowItWorks() {
                     src="https://images.unsplash.com/photo-1580893246395-52aead8960dc?crop=entropy&amp;cs=tinysrgb&amp;fm=jpg&amp;ixid=MnwzMzIzMzB8MHwxfHNlYXJjaHwxMHx8YWdyZWVtZW50fGVufDB8fHx8MTY3NzE2NjA3Ng&amp;ixlib=rb-4.0.3&amp;q=80&amp;w=1920"
                     alt=""
                   />
-                  <div className="absolute bottom-0 left-0 w-full px-11 pb-10">
+                  <div className="absolute bottom-0 left-0 w-full px-2 py-6 lg:px-11 lg:py-10">
                     <div
-                      className="px-8 py-4 bg-white bg-opacity-10 rounded-xl shadow-5xl"
+                      className="px-2 py-2 lg:px-8 lg:py-4 bg-white bg-opacity-10 rounded-xl shadow-5xl"
                       style={{
                         webkitBackdropFilter: "blur(5px)",
                         backdropFilter: "blur(5px)",
                       }}
                     >
-                      <h3 className="text-lg text-white text-center font-semibold">
+                      <h3 className="text-sm lg:text-lg text-white text-center font-semibold">
                         Make a Bartr with someone and learn a new skill!
                       </h3>
                     </div>
